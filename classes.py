0cf550fade2fe1fb0f73b0bb8214b44a5dcf65ea
Lucas Emil Bülow Mortensen <lucas37c@gmail.com>
2025-12-11 16:55:24 +0100

    adds classes template for changing to json format output

---
+++ classes.py
@@ -0,0 +1,25 @@
+from google import genai
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+class Ingredient(BaseModel):
+    name: str = Field(description="Name of the ingredient.")
+    quantity: str = Field(description="Quantity of the ingredient, including units.")
+
+class Recipe(BaseModel):
+    recipe_name: str = Field(description="The name of the recipe.")
+    prep_time_minutes: Optional[int] = Field(description="Optional time in minutes to prepare the recipe.")
+    ingredients: List[Ingredient]
+    instructions: List[str]
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents=prompt,
+    config={
+        "response_mime_type": "application/json",
+        "response_json_schema": Recipe.model_json_schema(),
+    },
+)
+
+recipe = Recipe.model_validate_json(response.text)
+print(recipe)
